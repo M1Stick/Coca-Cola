ece2563656811fc4919e2a7c0b2f086cec8b1108
M1Stick <sauronchik26@gmail.com>
2020-07-16 12:19:19 +0300

    started pack for coca-cola

---
+++ #src/js/script.js
@@ -0,0 +1,27 @@
+// $(document).ready(function() {
+//   $('.header__burger').click(function() {
+//     $('.header__menu').css('width','22%');
+//     $('body').toggleClass('lock');
+//   })
+// });
+// $(document).ready(function() {
+//   $('#close').click(function() {
+//     $('.header__menu').css('width','0');
+//     $('body').removeClass('lock');
+//   })
+// });
+// $(document).ready(function() {
+//   $('.header__popup-close').click(function() {
+//     $('body').removeClass('lock');
+//   })
+// });
+// $(document).bind( 'mousewheel', function (e) { 
+//   let nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100); 
+//   e.preventDefault(); 
+//   e.stopPropagation(); 
+//   $(document.body).stop().animate( { 
+//        scrollTop : nt 
+//    } , 500 , 'easeInOutCubic' );  
+// });
+
+// @@include('_slider.js')
